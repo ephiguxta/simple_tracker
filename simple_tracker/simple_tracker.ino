f585642c104de8de1da4b74b9cb04c358f958bf7
ephiguxta <fabiogustavo@tuta.io>
2024-09-23 15:48:27 -0400

    pegando horário, latitude e longitude de maneira "bruta"

---
+++ simple_tracker/simple_tracker.ino
@@ -0,0 +1,34 @@
+#include <TinyGPSPlus.h>
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;
+TinyGPSPlus gps;
+
+const uint8_t rx = 16;
+const uint8_t tx = 17;
+
+void setup() {
+  SerialBT.begin("telemetria");
+  delay(32);
+
+  uint8_t mac_addr[6];
+  SerialBT.getBtAddress(mac_addr);
+
+  Serial.begin(9600);
+  delay(32);
+
+  Serial1.begin(9600, SERIAL_8N1, rx, tx);
+  delay(32);
+}
+
+void loop() {
+  if(Serial1.available() > 0) {
+    gps.encode(Serial1.read());
+  }
+
+  Serial.printf("(%d:%d:%d) lat: %.6f lon: %.6f\n",
+    gps.time.hour(), gps.time.minute(), gps.time.second(),
+    gps.location.lat(), gps.location.lng());
+  
+  delay(500);
+}
