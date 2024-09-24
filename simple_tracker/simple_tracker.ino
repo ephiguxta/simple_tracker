a13881ce6e52c15af6c0200049a6448d23336497
ephiguxta <fabiogustavo@tuta.io>
2024-09-24 10:37:50 -0400

    trocando de bluetooth para wifi

--- simple_tracker/simple_tracker.ino
+++ simple_tracker/simple_tracker.ino
@@ -1,8 +1,8 @@
 #include <TinyGPSPlus.h>
 #include <string.h>
-#include "BluetoothSerial.h"
+#include <WiFi.h>
+#include "wifi.h"
 
-BluetoothSerial SerialBT;
 TinyGPSPlus gps;
 
 const uint8_t rx = 16;
@@ -11,14 +11,22 @@ const uint8_t tx = 17;
 bool check_lat_lng();
 
 void setup() {
-  SerialBT.begin("telemetria");
-  delay(32);
-
-  uint8_t mac_addr[6];
-  SerialBT.getBtAddress(mac_addr);
-
   Serial.begin(9600);
   delay(32);
+
+  delay(2000);
+
+  WiFi.enableSTA(true);
+  WiFi.begin(ssid, passwd);
+  delay(512);
+
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.printf("Esperando a conexão WiFi!\n");
+    delay(500);
+  }
+
+  Serial.printf("Conectou, meu IP é: %s\n",
+      WiFi.localIP().toString().c_str());
 
   Serial1.begin(9600, SERIAL_8N1, rx, tx);
   delay(32);
@@ -40,15 +48,18 @@ void loop() {
 
   if (check_lat_lng()) {
     uint8_t msg_size = strlen((const char *)msg);
-    SerialBT.write(msg, msg_size);
+    Serial.printf("%s", msg);
+    // SerialBT.write(msg, msg_size);
     delay(256);
   }
 }
 
 bool check_lat_lng() {
+  // FIXME: inverta a lógica, atualmente está dessa forma apenas
+  // para testes.
   if (gps.location.lat() == 0 || gps.location.lng() == 0) {
-    return false;
+    return true;
   }
 
-  return true;
+  return false;
 }
