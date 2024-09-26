d8db7ee1cd214935125a54776d97229ca8442330
ephiguxta <fabiogustavo@tuta.io>
2024-09-26 15:15:39 -0400

    enviando os dados para o telegram

--- simple_tracker/simple_tracker.ino
+++ simple_tracker/simple_tracker.ino
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <HTTPClient.h>
 #include <WiFi.h>
+#include <NetworkClientSecure.h>
 #include "wifi.h"
 #include "tg_credentials.h"
 
@@ -11,6 +12,27 @@ const uint8_t rx = 16;
 const uint8_t tx = 17;
 
 bool check_lat_lng();
+void connect_wifi();
+bool send_data2tg(const char msg[64]);
+
+void set_clock() {
+  configTime(0, 0, "pool.ntp.org");
+
+  Serial.print(F("Waiting for NTP time sync: "));
+  time_t nowSecs = time(nullptr);
+  while (nowSecs < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(F("."));
+    yield();
+    nowSecs = time(nullptr);
+  }
+
+  Serial.println();
+  struct tm timeinfo;
+  gmtime_r(&nowSecs, &timeinfo);
+  Serial.print(F("Current time: "));
+  Serial.print(asctime(&timeinfo));
+}
 
 void setup() {
   Serial.begin(9600);
@@ -32,10 +54,12 @@ void setup() {
 
   Serial1.begin(9600, SERIAL_8N1, rx, tx);
   delay(32);
+
+  set_clock();
 }
 
 void loop() {
-  uint8_t msg[64] = { 0 };
+  char msg[64] = { 0 };
 
   if (Serial1.available() > 0) {
     char data = Serial1.read();
@@ -44,15 +68,15 @@ void loop() {
     delay(16);
   }
 
-  snprintf((char *)msg, 64, "(%d:%d:%d) lat: %.6f lon: %.6f\n",
+  snprintf(msg, 64, "[%d:%d:%d] lat: %.6f lon: %.6f",
            gps.time.hour(), gps.time.minute(), gps.time.second(),
            gps.location.lat(), gps.location.lng());
 
   if (check_lat_lng()) {
-    uint8_t msg_size = strlen((const char *)msg);
-    Serial.printf("%s", msg);
+    uint8_t msg_size = strlen(msg);
+    Serial.printf("%s\n", msg);
     // SerialBT.write(msg, msg_size);
-    send_data2tg();
+    send_data2tg(msg);
     delay(256);
   }
 }
@@ -64,17 +88,59 @@ bool check_lat_lng() {
     return true;
   }
 
-  return false;
+  return true;
 }
 
-bool send_data2tg() {
-  char post_url[128] = { 0 };
-  WiFiClient client;
-  HTTPClient http;
+bool send_data2tg(const char msg[64]) {
+  char url[256] = { 0 };
+  HTTPClient https;
 
-  http.begin(client, endpoint);
-  const char *params = ""
-  snprintf(post_url, 75, "%s/bot%s", endpoint, key);
-  
+  if(WiFi.status() == WL_CONNECTED) {
+    delay(3000);
+    NetworkClientSecure *client = new NetworkClientSecure;
+    
+    if(client) {
+      client->setCACert(rootCACertificate);
+      snprintf(url, 78, "%s/bot%s", endpoint, key);
+
+      strcat(url, "/sendMessage");
+
+      // montar o parâmetro de ID do chat 
+      char chat_param[16] = "?chat_id=";
+      strncat(chat_param, chat_id, strlen(chat_id));
+
+      strcat(url, chat_param);
+
+      char text[128] = "&text=";
+      strcat(text, msg);
+      strcat(url, text);
+
+      Serial.printf("(%s)\n", url);
+
+      if(https.begin(*client, url)) {
+
+        int https_code = https.GET();
+
+        if(https_code == HTTP_CODE_OK) {
+          Serial.printf("Mensagem Ok!");
+        } else {
+          Serial.printf("%s\n", https.errorToString(https_code).c_str());
+        }
+
+        delay(128);
+        https.end();
+      }
+
+    } else {
+      Serial.printf("O WiFi não está conectado, reiniciando o ESP32\n");
+      delay(100);
+      ESP.restart();
+    }
+
+    delay(1e3 * 60);
+   
+    delete client;
+  }
+
   return true;
 }
