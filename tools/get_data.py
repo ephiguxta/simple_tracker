d0c15c4c59adc87a0317b225ed11db939e07d818
ephiguxta <fabiogustavo@tuta.io>
2024-09-29 09:54:02 -0400

    enviando os dados parta o bot do telegram

--- tools/get_data.py
+++ tools/get_data.py
@@ -9,32 +9,38 @@ from sentence_list import sentence_list
 config = dotenv_values(".env")
 url = f'https://api.telegram.org/bot{config["token"]}/sendMessage'
 
+ser = serial.Serial("/dev/ttyUSB0", 9600)
+
 while True:
+    valid_data_re = re.compile(r"^\$([A-Z0-9\.\,\-\$\*])+")
     tags = []
 
-    with serial.Serial('/dev/ttyUSB0', 9600) as ser:
-        line = ''
-        line = ser.read(256)
-        try:
-            line = line.decode("utf-8")
-        except:
-            line = ''
+    # lê a linha e remove CRLF
+    gps_line = ser.readline()
+    gps_line = gps_line[:-2]
 
-        print(f"[{line}]")
+    try:
+        gps_line = gps_line.decode("utf-8")
+        match = valid_data_re.match(gps_line)
 
-        for tag in sentence_list(line):
-            tags.append(tag)
+        if match is not None:
+            tag_regex = re.compile(r"^(\$[A-Z]{5})")
+            match = tag_regex.match(gps_line)
 
-            if tag != 'GPGSV':
-                count = tags.count(tag)
-                if count >= 2:
-                    data = {
-                        'chat_id': config["chat_id"],
-                        'text': line
-                    }
+            # TODO: fazer um préprocessamento nos dados para enviar
+            # apenas quando houver latitude, longitude e altitude
+            if match is not None and (
+                match[1] == "$GNRMC" or match[1] == "$GPGGA"
+            ):
+                data = {"chat_id": config["chat_id"], "text": gps_line}
+                req = requests.post(url, data=data)
+                if req.status_code != requests.codes.ok:
+                    print("erro!")
+                
+                time.sleep(60)
+    except UnicodeDecodeError:
+        print("deu ruim!")
 
-                    req = requests.post(url, data=data)
-                    if req.status_code != requests.status.ok:
-                        print("erro!")
-                        
-                    time.sleep(60)
+
+def get_tag(gps_line):
+    regex_tag = re.compile(r"")
