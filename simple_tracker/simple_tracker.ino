3fddccc85d7b56022895954f6acfe442034db778
ephiguxta <fabiogustavo@tuta.io>
2024-09-29 14:27:32 -0400

    realizando a leitura dos bytes que vem do TX do NEO6M e verificando se formam uma linha válida

--- simple_tracker/simple_tracker.ino
+++ simple_tracker/simple_tracker.ino
@@ -1,19 +1,19 @@
-#include <TinyGPSPlus.h>
 #include <string.h>
 #include "BluetoothSerial.h"
 
 BluetoothSerial SerialBT;
-TinyGPSPlus gps;
 
 const uint8_t rx = 16;
 const uint8_t tx = 17;
 
-bool check_lat_lng();
+//bool check_lat_lng();
+bool valid_char(char data);
 
 void setup() {
   SerialBT.begin("telemetria");
   delay(32);
 
+  // TODO: utilizar isso como sufixo do nome do dispositivo (com salt)
   uint8_t mac_addr[6];
   SerialBT.getBtAddress(mac_addr);
 
@@ -25,15 +25,30 @@ void setup() {
 }
 
 void loop() {
-  uint8_t msg[64] = { 0 };
+  char msg[128] = { 0 };
+  Serial1.setTimeout(1000);
 
   if (Serial1.available() > 0) {
-    char data = Serial1.read();
-    //Serial.printf("%c", data);
-    gps.encode(data);
-    delay(16);
+    char data;
+
+    for (uint8_t i = 0; i < 128; i++) {
+      data = Serial1.read();
+
+      // se o dado não for um caractere válido e não iniciar com '$'
+      if ((i == 0 && data != '$') || !valid_char(data)) {
+        msg[i] = '\0';
+        break;
+      }
+
+      msg[i] = data;
+      delay(0.2);
+    }
   }
 
+  Serial.printf("%s\n", msg);
+  delay(0.5);
+
+  /*
   snprintf((char *)msg, 64, "(%d:%d:%d) lat: %.6f lon: %.6f\n",
            gps.time.hour(), gps.time.minute(), gps.time.second(),
            gps.location.lat(), gps.location.lng());
@@ -43,8 +58,25 @@ void loop() {
     SerialBT.write(msg, msg_size);
     delay(256);
   }
+  */
 }
 
+bool valid_char(char data) {
+  if (data >= 'A' && data <= 'Z') {
+    return true;
+  }
+
+  if (data >= '0' && data <= '9') {
+    return true;
+  }
+
+  if (data == '*' || data == '.' || data == ',' || data == '$') {
+    return true;
+  }
+
+  return false;
+}
+/*
 bool check_lat_lng() {
   if (gps.location.lat() == 0 || gps.location.lng() == 0) {
     return false;
@@ -52,3 +84,4 @@ bool check_lat_lng() {
 
   return true;
 }
+*/
